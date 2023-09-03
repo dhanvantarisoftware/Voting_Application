f4797084e102b02aa3359556688fc4286f3e0dc3
dhanvantarisoftware <dhanvantari.software.trngs@gmail.com>
2023-09-03 22:41:35 +0530

    Docker-Compose

--- result/views/app.js
+++ result/views/app.js
@@ -1,4 +1,4 @@
-var app = angular.module('catsvsdogs', []);
+var app = angular.module('maheshvsgautham', []);
 var socket = io.connect({transports:['polling']});
 
 var bg1 = document.getElementById('background-stats-1');
